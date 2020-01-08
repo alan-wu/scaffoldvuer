08f96284555e39e3d1e9bf0be50abc78c3e003c3
alan-wu <alan.wu@auckland.ac.nz>
2020-01-09 11:57:58 +1300

    Separate list of primitives.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <ScaffoldVuer url="https://mapcore-bucket1.s3-us-west-2.amazonaws.com/ISAN/scaffold/use_case4/rat_heart_metadata.json" ref="scaffold" showColourPicker/>
+    <ScaffoldVuer url="https://mapcore-bucket1.s3-us-west-2.amazonaws.com/others/a3/a3_metadata.json" ref="scaffold" showColourPicker/>
   </div>
 </template>
 
--- src/components/ScaffoldVuer.vue
+++ src/components/ScaffoldVuer.vue
@@ -1,25 +1,57 @@
 <template>
   <div class="scaffold-container">
     <div style="height:100%;width:100%;position:relative">
-      <div id="organsDisplayArea" style="height:100%;width:100%;" ref="display">
-      </div>
+      <div id="organsDisplayArea" style="height:100%;width:100%;" ref="display"></div>
       <div class="check-list">
         <el-row style="margin-bottom:10px;">
           <el-button type="success" @click="viewAll" round>View all</el-button>
         </el-row>
-        <el-checkbox-group v-model="checkboxGroup" size="small">
-        <el-row v-for="item in orderedGroups" :key="item">
-          <el-container>
-            <el-checkbox style="margin-top:3px;" :label="item" @change="visibilityToggle(item, $event)" :checked=true border>{{item}}</el-checkbox>
-            <el-color-picker v-if="showColourPicker" style="margin-top:3px;" :value="colour(item)" @change="colourChanged(item, $event)" size="small"></el-color-picker>
-          </el-container>
-        </el-row>
-        </el-checkbox-group>
+        <el-collapse v-model="activeCollapseItems">
+          <el-collapse-item
+            v-for="(group, type) in primitivesList"
+            :title="type"
+            :key="type"
+            :name="type"
+          >
+            <el-checkbox-group v-model="group.checkbox" size="small">
+              <el-row v-for="item in orderedList(group.primitives)" :key="item">
+                <el-container>
+                  <el-checkbox
+                    style="margin-top:3px;"
+                    :label="item"
+                    @change="visibilityToggle(item, $event, type)"
+                    :checked="true"
+                    border
+                  >{{item}}</el-checkbox>
+                  <el-color-picker
+                    v-if="showColourPicker&&colour(type, item)"
+                    style="margin-top:3px;"
+                    :value="colour(type, item)"
+                    @change="colourChanged(type, item, $event)"
+                    size="small"
+                  ></el-color-picker>
+                </el-container>
+              </el-row>
+            </el-checkbox-group>
+          </el-collapse-item>
+        </el-collapse>
       </div>
       <div class="timeSlider" v-if="sceneData.timeVarying">
-        <el-button type="success" v-if="isPlaying" @click="play(false)" icon="el-icon-video-pause" round>Pause</el-button>
+        <el-button
+          type="success"
+          v-if="isPlaying"
+          @click="play(false)"
+          icon="el-icon-video-pause"
+          round
+        >Pause</el-button>
         <el-button type="success" v-else @click="play(true)" icon="el-icon-video-play" round>Play</el-button>
-        <el-slider :min=0 :max=100 :value="sceneData.currentTime" :step=0.1 @input="timeChange($event)"></el-slider>
+        <el-slider
+          :min="0"
+          :max="100"
+          :value="sceneData.currentTime"
+          :step="0.1"
+          @input="timeChange($event)"
+        ></el-slider>
       </div>
     </div>
   </div>
@@ -28,35 +60,57 @@
 <script>
 /* eslint-disable no-alert, no-console */
 import Vue from "vue";
-import { Button, Checkbox, CheckboxGroup, ColorPicker, Container, Row, Slider } from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import lang from 'element-ui/lib/locale/lang/en';
-import locale from 'element-ui/lib/locale';
-var orderBy = require('lodash/orderBy');
-
+import {
+  Button,
+  Checkbox,
+  CheckboxGroup,
+  Collapse,
+  CollapseItem,
+  ColorPicker,
+  Container,
+  Row,
+  Slider
+} from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import lang from "element-ui/lib/locale/lang/en";
+import locale from "element-ui/lib/locale";
+var orderBy = require("lodash/orderBy");
 locale.use(lang);
 Vue.use(Button);
 Vue.use(Checkbox);
 Vue.use(CheckboxGroup);
 Vue.use(ColorPicker);
+Vue.use(Collapse);
+Vue.use(CollapseItem);
 Vue.use(Container);
 Vue.use(Row);
 Vue.use(Slider);
 
-const OrgansViewer = require('physiomeportal/src/modules/organsRenderer').OrgansViewer;
-const EventNotifier = require("physiomeportal/src/utilities/eventNotifier").EventNotifier;
+const OrgansViewer = require("physiomeportal/src/modules/organsRenderer")
+  .OrgansViewer;
+const EventNotifier = require("physiomeportal/src/utilities/eventNotifier")
+  .EventNotifier;
 
 const eventNotifierCallback = function(component) {
   return function(event) {
     if (event.eventType == 1)
-      component.$emit('scaffold-selected', event.identifiers);
+      component.$emit("scaffold-selected", event.identifiers);
     else if (event.eventType == 2)
-      component.$emit('scaffold-highlighted', event.identifiers);
-  }
-}
+      component.$emit("scaffold-highlighted", event.identifiers);
+  };
+};
+
+const getPrimitivesListWithGroupName = function(scene, type, name) {
+  let array = [];
+  if (type == "Geometries") array = scene.findGeometriesWithGroupName(name);
+  else if (type == "Lines") array = scene.findLinesWithGroupName(name);
+  else if (type == "Glyphsets") array = scene.findGlyphsetsWithGroupName(name);
+  else if (type == "Pointsets") array = scene.findPointsetsWithGroupName(name);
+  return array;
+};
 
 export default {
-  name: 'ScaffoldVuer',
+  name: "ScaffoldVuer",
   beforeCreate: function() {
     let eventNotifier = new EventNotifier();
     eventNotifier.subscribe(this, eventNotifierCallback(this));
@@ -67,71 +121,102 @@ export default {
     viewAll: function() {
       this.$module.viewAll();
     },
-    visibilityToggle: function(item, event) {
-      this.$module.changeOrganPartsVisibility(item, event);
+    visibilityToggle: function(item, event, type) {
+      this.$module.changeOrganPartsVisibility(item, event, type.toLowerCase());
     },
     timeChange: function(event) {
-      if (event != this.sceneData.currentTime)
-        this.$module.updateTime(event);
+      if (event != this.sceneData.currentTime) this.$module.updateTime(event);
     },
     play: function(flag) {
       this.$module.playAnimation(flag);
       this.isPlaying = flag;
     },
-    colourChanged: function(name, colour) {
-      let array = this.$module.scene.findGeometriesWithGroupName(name);
-      let hexString = colour.replace('#', '0x');
-      array.forEach(geometry => geometry.morph.material.color.setHex(hexString));
+    colourChanged: function(type, name, colour) {
+      let array = getPrimitivesListWithGroupName(
+        this.$module.scene,
+        type,
+        name
+      );
+      let hexString = colour.replace("#", "0x");
+      array.forEach(object => object.setColourHex(hexString));
       this.lastColourChanged = name;
     },
-    colour: function (name) {
-        let array = this.$module.scene.findGeometriesWithGroupName(name);
-        return '#' + array[0].morph.material.color.getHexString();
+    colour: function(type, name) {
+      let array = getPrimitivesListWithGroupName(
+        this.$module.scene,
+        type,
+        name
+      );
+      if (array.length > 0) return "#" + array[0].getColourHex();
+      return undefined;
     }
   },
-  props: {'url': String,
-          'showColourPicker': Boolean},
+  props: { url: String, showColourPicker: Boolean },
   data: function() {
     return {
       sceneData: this.$module.sceneData,
       isPlaying: false,
       step: 0.1,
-      checkboxGroup: []
-    }
+      primitivesList: {
+        Geometries: {
+          checkbox: [],
+          primitives: this.$module.sceneData.geometries
+        },
+        Lines: {
+          checkbox: [],
+          primitives: this.$module.sceneData.lines
+        },
+        Glyphsets: {
+          checkbox: [],
+          primitives: this.$module.sceneData.glyphsets
+        },
+        Pointsets: {
+          checkbox: [],
+          primitives: this.$module.sceneData.pointsets
+        }
+      },
+      activeCollapseItems: []
+    };
   },
   computed: {
-    orderedGroups: function () {
-      return orderBy(this.sceneData.groups);
+    orderedList: function() {
+      return function(primitives) {
+        return orderBy(primitives);
+      };
     }
   },
-  mounted: function () {
-    this.$module.loadOrgansFromURL(this.url, undefined, undefined, "Overlay", undefined);
+  mounted: function() {
+    this.$module.loadOrgansFromURL(
+      this.url,
+      undefined,
+      undefined,
+      "Overlay",
+      undefined
+    );
     this.$module.initialiseRenderer(this.$refs.display);
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 .scaffold-container {
-  height:100%;
-  width:100%;
+  height: 100%;
+  width: 100%;
 }
 
 .check-list {
   position: absolute;
-  top:10px;
+  top: 10px;
   left: 10px;
-  text-align:left;
+  text-align: left;
 }
 
 .timeSlider {
   position: absolute;
   left: 2.5%;
   height: 48px;
-  width:95%;
+  width: 95%;
   bottom: 40px;
 }
-
 </style>
