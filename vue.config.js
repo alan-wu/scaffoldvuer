8597f0ae689f48d42c5472b44779072f08ab3054
alan-wu <alan.wu@auckland.ac.nz>
2020-09-03 14:16:06 +1200

    Make any font-face inline in the css.

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,18 @@ module.exports = {
         .use('raw-loader')
           .loader('raw-loader')
           .end()
+
+      const fontsRule = config.module.rule('fonts')
+      fontsRule.uses.clear()
+      config.module
+        .rule('fonts')
+        .test(/\.(ttf|otf|eot|woff|woff2)$/)
+        .use('base64-inline-loader')
+        .loader('base64-inline-loader')
+        .tap(options => {
+          // modify the options...    
+          return options
+        })
+        .end()
     }
 }
