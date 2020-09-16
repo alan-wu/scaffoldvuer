b4f22486d5253d5f0f84343120d050d33909bb83
alan-wu <alan.wu@auckland.ac.nz>
2020-09-16 14:53:14 +1200

    Build bundle without external modules.

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,30 @@
-module.exports = {
-    chainWebpack: config => {
-      // GraphQL Loader
-      config.module
-        .rule('shader')
-        .test(/\.(vs|fs)$/i)
-        .use('raw-loader')
-          .loader('raw-loader')
-          .end()
+const nodeExternals = require('webpack-node-externals');
 
-      const fontsRule = config.module.rule('fonts')
-      fontsRule.uses.clear()
-      config.module
-        .rule('fonts')
-        .test(/\.(ttf|otf|eot|woff|woff2)$/)
-        .use('base64-inline-loader')
-        .loader('base64-inline-loader')
-        .tap(options => {
-          // modify the options...    
-          return options
-        })
-        .end()
+module.exports = {
+  configureWebpack: config => {
+    if(process.env.NODE_ENV === 'production') {
+      config.externals =  [ nodeExternals({allowlist: [/^physiomeportal/]}) ];
     }
+  },
+  chainWebpack: config => {
+    // GraphQL Loader
+    config.module
+      .rule('shader')
+      .test(/\.(vs|fs)$/i)
+      .use('raw-loader')
+        .loader('raw-loader')
+        .end()
+    const fontsRule = config.module.rule('fonts')
+    fontsRule.uses.clear()
+    config.module
+      .rule('fonts')
+      .test(/\.(ttf|otf|eot|woff|woff2)$/)
+      .use('base64-inline-loader')
+      .loader('base64-inline-loader')
+      .tap(options => {
+        // modify the options...    
+        return options
+      })
+      .end()
+  },
 }
