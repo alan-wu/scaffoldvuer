3ed8bfcc16445b3c9af9d35d13d0556dd0badfea
alan-wu <alan.wu@auckland.ac.nz>
2021-02-26 11:36:04 +1300

    Fix formatting.

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,17 +1,17 @@
 module.exports = {
 	// set your styleguidist configuration here
-	title: 'Scaffoldvuer API reference',
-	 components: 'src/components/ScaffoldVuer.vue',
-	// defaultExample: true,
-	// sections: [
-	//   {
-	//     name: 'First Section',
-	//     components: 'src/components/**/[A-Z]*.vue'
-	//   }
-	// ],
-	// webpackConfig: {
-	//   // custom config goes here
-	// },
+title: 'Scaffoldvuer API reference',
+   components: 'src/components/ScaffoldVuer.vue',
+  // defaultExample: true,
+  // sections: [
+  //   {
+  //     name: 'First Section',
+  //     components: 'src/components/**/[A-Z]*.vue'
+  //   }
+  // ],
+  // webpackConfig: {
+  //   // custom config goes here
+  // },
   exampleMode: 'expand',
   copyCodeButton: true,
   ribbon: {
