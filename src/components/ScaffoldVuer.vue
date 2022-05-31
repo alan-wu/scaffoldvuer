e735137e135a51bc3435eec737a62a8a8d16b911
alan-wu <alan.wu@auckland.ac.nz>
2022-05-31 16:45:54 +1200

    Improve positioning of tooltip of region.

--- src/components/ScaffoldVuer.vue
+++ src/components/ScaffoldVuer.vue
@@ -760,7 +760,7 @@ export default {
         this.$emit("scaffold-selected", event.identifiers);
       } else if (event.eventType == 2) {
         this.tData.visible = false;
-        const offsets = this.$refs.scaffoldContainer.getBoundingClientRect();
+       // const offsets = this.$refs.scaffoldContainer.getBoundingClientRect();
         if (event.identifiers[0]) {
           let id = event.identifiers[0].data.id
             ? event.identifiers[0].data.id
@@ -768,8 +768,8 @@ export default {
           if (event.identifiers[0].coords) {
             this.tData.visible = true;
             this.tData.label = id;
-            this.tData.x = event.identifiers[0].coords.x - offsets.left;
-            this.tData.y = event.identifiers[0].coords.y - offsets.top;
+            this.tData.x = event.identifiers[0].coords.x;
+            this.tData.y = event.identifiers[0].coords.y;
           }
           if (this.$refs.treeControl) {
             let region = event.identifiers[0].data.region;
