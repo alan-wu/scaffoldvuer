30b932400caa6d0c70fce039b1b2cef73b22ab7e
alan-wu <alan.wu@auckland.ac.nz>
2024-04-24 13:05:39 +1200

    Update dependencies.

--- src/components.d.ts
+++ src/components.d.ts
@@ -17,7 +17,6 @@ declare module 'vue' {
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElIconArrowLeft: typeof import('@element-plus/icons-vue')['ArrowLeft']
     ElIconArrowRight: typeof import('@element-plus/icons-vue')['ArrowRight']
-    ElIconClose: typeof import('@element-plus/icons-vue')['Close']
     ElIconDelete: typeof import('@element-plus/icons-vue')['Delete']
     ElIconPlus: typeof import('@element-plus/icons-vue')['Plus']
     ElIconWarningFilled: typeof import('@element-plus/icons-vue')['WarningFilled']
