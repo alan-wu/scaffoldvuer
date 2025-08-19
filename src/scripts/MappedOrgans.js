6c4f982e262bb95d80bae3e87cb3288dd3830db2
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-08-19 13:44:19 +1200

    Remove unnecessary ids from mapped organs

--- src/scripts/MappedOrgans.js
+++ src/scripts/MappedOrgans.js
@@ -2,47 +2,38 @@ const organsWithAnatomicalIds = [
   {
       "label": "urinary bladder",
       "anatomicalId": "UBERON:0001255",
-      "id": "re11/pr2511",
   },
   {
       "label": "brainstem",
       "anatomicalId": "UBERON:0002298",
-      "id": "re7/pr2503",
   },
   {
       "label": "caecum",
       "anatomicalId": "UBERON:0001153",
-      "id": "re4/pr2012",
   },
   {
       "label": "colon",
       "anatomicalId": "UBERON:0001155",
-      "id": "re4/pr2536",
   },
   {
       "label": "esophagus",
       "anatomicalId": "UBERON:0001043",
-      "id": "re4/pr2013",
   },
   {
       "label": "small intestine",
       "anatomicalId": "UBERON:0002108",
-      "id": "re4/pr2535",
   },
   {
       "label": "stomach",
       "anatomicalId": "UBERON:0000945",
-      "id": "re4/pr2014",
   },
   {
       "label": "heart",
       "anatomicalId": "UBERON:0000948",
-      "id": "re8/pr2504",
   },
   {
       "label": "lung",
       "anatomicalId": "UBERON:0002048",
-      "id": "re5/pr2006",
   },
 ];
 
