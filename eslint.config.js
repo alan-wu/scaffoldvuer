c0f62106c811194c552722358f757226ef3af28a
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-09-14 15:44:37 +1200

    Update ESLint config with prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,46 @@
-import js from "@eslint/js";
-import vue from "eslint-plugin-vue";
-import globals from "globals";
+import js from '@eslint/js';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import vue from 'eslint-plugin-vue';
+import globals from 'globals';
 
 export default [
   {
-    ignores: ["dist/**", "docs/.vitepress/**", "docs/components/**"],
+    ignores: ['dist/**', 'docs/.vitepress/**', 'docs/components/**'],
   },
   js.configs.recommended,
-  ...vue.configs["flat/essential"],
+  ...vue.configs['flat/essential'],
+  prettierConfig,
   {
-    files: ["**/*.{js,vue}"],
+    files: ['**/*.{js,vue}'],
+    plugins: {
+      prettier,
+    },
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: {
         ...globals.node,
         ...globals.browser,
       },
     },
-    rules: {},
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          trailingComma: 'all',
+        },
+      ],
+      'arrow-body-style': 'off',
+      'prefer-arrow-callback': 'off',
+      'no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
   },
 ];
