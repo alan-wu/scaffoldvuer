3fe3592ebcfd360b52fa3ab61d569f0b682b31a1
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-09-15 12:33:36 +1200

    Use ElementPlus Autoimport

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
@@ -18,6 +19,9 @@ export default defineConfig(({ command, mode }) => {
     },
     plugins: [
       vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
       Components({
         // allow auto load markdown components under `./src/components/`
         extensions: ['vue', 'md'],
@@ -30,9 +34,6 @@ export default defineConfig(({ command, mode }) => {
         ],
         dts: 'src/components.d.ts',
       }),
-
-      // https://github.com/antfu/unocss
-      // see unocss.config.ts for config
     ],
     resolve: {
       alias: {
